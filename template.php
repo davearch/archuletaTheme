07e74799eba4bfcc1f9c93415bdb34a302e4e858
davearch <davearch@email.arizona.edu>
2014-06-10 22:48:37 -0700

    uggh

--- template.php
+++ template.php
@@ -11,7 +11,7 @@ function archuletaTheme_js_alter(&$javascript) {
 
     // update jquery for non-administrative pages
     if (!(module_exists('jquery_update'))) {
-    	if (arg(0) != 'admin' && arg(0) != 'panels' && arg(0) != 'ctools' {
+    	if (arg(0) != 'admin' && arg(0) != 'panels' && arg(0) != 'ctools') {
 		$jquery_file = drupal_get_path('theme', 'archuletaTheme');
         	$jquery_file .= '/js/jquery-1.11.1.min.js';
         	$jquery_version = '1.11.1';
