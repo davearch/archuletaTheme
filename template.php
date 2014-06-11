eb776c9c75993d1d750ac18bd4faaa0dbe228860
davearch <davearch@email.arizona.edu>
2014-06-10 22:14:31 -0700

    more template work

--- template.php
+++ template.php
@@ -1,5 +1,12 @@
 <?php
 
+function archuletaTheme_preprocess_html(&$vars) {
+    // change doctype to html5
+    $vars['doctype'] = '<!DOCTYPE html>' . "\n";
+    // create variable containing the path to the theme
+    $vars['path'] = drupal_get_path('theme', 'archuletaTheme');
+}
+
 function archuletaTheme_js_alter(&$javascript) {
 
     // update jquery for non-administrative pages
@@ -16,3 +23,4 @@ function archuletaTheme_js_alter(&$javascript) {
 	}
     }
 }
+
